2d51e02ff1c981e4ca6fd17442105ca6c7fdfd39
pporeba <poreba__p@wp.pl>
2026-03-16 11:49:14 +0100

    Implementation of diffrent solution

--- IntroClasses/Program.cs
+++ IntroClasses/Program.cs
@@ -6,26 +6,6 @@ hero.Display();
 // Console.ReadKey(true);
 while (isPlaying)
 {
-    ConsoleKeyInfo input = Console.ReadKey(true);
-    switch (input.Key)
-    {
-        case ConsoleKey.S:
-            hero.Move(0, 1);
-            break;
-        case ConsoleKey.W:
-            hero.Move(0, -1);
-            break;
-        case ConsoleKey.A:
-            hero.Move(-1, 0);
-            break;
-        case ConsoleKey.D:
-            hero.Move(1, 0);
-            break;
-        case ConsoleKey.Q:
-            isPlaying = false;
-            break;
-    }
-
-    hero.Display();
+    isPlaying = hero.TakeTurn();
 }
 Console.WriteLine("Goodbye");
