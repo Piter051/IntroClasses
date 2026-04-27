5e4a46492b801b851547f6bd14900ac636ff24a1
pporeba <poreba__p@wp.pl>
2026-04-27 11:48:15 +0200

    Add Vector 2 struct

--- IntroClasses/Player.cs
+++ IntroClasses/Player.cs
@@ -2,26 +2,30 @@ namespace IntroClasses;
 
 public class Player
 {
-    private int _x;
-    private int _y;
+    private Vector2 _position = new Vector2(0, 0);
     private string _avatar = "@";
+
+    public Player(Vector2 startingPosition)
+    {
+        _position = startingPosition;
+    }
 
     public void Display()
     {
-        Console.SetCursorPosition(_x,_y);
+        Console.SetCursorPosition(_position.X,_position.Y);
         Console.Write(_avatar);
     }
 
     public void Move(int diffX, int diffY)
     {
-        int targetX = _x + diffX;
-        int targetY = _y + diffY;
+        int targetX = _position.X + diffX;
+        int targetY = _position.Y + diffY;
         
 
         if (targetX >= 0 && targetX < Console.BufferWidth && targetY >= 0 && targetY < Console.BufferHeight) 
         {
-            _x = targetX;
-            _y = targetY;
+            _position.X = targetX;
+            _position.Y = targetY;
         }
     }
 
@@ -29,7 +33,7 @@ public class Player
     {
         bool isPlaying = true;
         ConsoleKeyInfo input = Console.ReadKey(true);
-        Console.SetCursorPosition(_x, _y);
+        Console.SetCursorPosition(_position.X, _position.Y);
         Console.Write(" ");
         switch (input.Key)
         {
--- IntroClasses/Program.cs
+++ IntroClasses/Program.cs
@@ -1,11 +1,18 @@
 ﻿using IntroClasses;
 
 bool isPlaying = true;
-Player hero = new Player();
+Vector2 startingPosition = new Vector2(4, 2);
+Player hero = new Player(startingPosition);
+startingPosition.X = 0;
+startingPosition.Y = 0;
+    
+//startingPosition = new Vector2(0, 0);
+Player anotherHero = new Player(startingPosition);
 hero.Display();
 // Console.ReadKey(true);
 while (isPlaying)
 {
     isPlaying = hero.TakeTurn();
+    isPlaying = anotherHero.TakeTurn();
 }
 Console.WriteLine("Goodbye");
